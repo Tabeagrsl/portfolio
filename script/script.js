8ccfdbd4fcef01cd1e8c664b9eef708ac256826c
Tabea Grösel <tabeagrosel@Tabeas-MacBook-Air.local>
2019-01-06 22:52:44 +0100

    added pictures & descriptions, started with media queries, changed homepage

---
+++ script/script.js
@@ -0,0 +1,24 @@
+
+
+(function () {
+       var burger = document.querySelector('.nav-toggle');
+       var menu = document.querySelector('.navlist');
+       burger.addEventListener('click', function () {
+           burger.classList.toggle('is-active');
+           menu.classList.toggle('is-active');
+       });
+   })();
+
+let whale =document.querySelector("#whale");
+whale.addEventListener("click", fade);
+
+    function fade () {
+        whale.classList.toggle("fade");
+    }
+
+let mirror =document.querySelector("#mirror");
+mirror.addEventListener("click", kick);
+
+    function kick(){
+        mirror.classList.toggle("kick");
+    }
