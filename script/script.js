edfee348413af6ac38d769b988f8c3b97d2762c4
Tabea Grösel <tabeagrosel@Tabeas-MacBook-Air.local>
2019-01-07 19:40:56 +0100

    validated css, responsive with grids, working on most pages.

--- script/script.js
+++ script/script.js
@@ -1,24 +1,25 @@
-
-
 (function () {
-       var burger = document.querySelector('.nav-toggle');
-       var menu = document.querySelector('.navlist');
-       burger.addEventListener('click', function () {
-           burger.classList.toggle('is-active');
-           menu.classList.toggle('is-active');
-       });
-   })();
+    var burger = document.querySelector('.nav-toggle');
+    var menu = document.querySelector('.navlist');
+    burger.addEventListener('click', function () {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
 
-let whale =document.querySelector("#whale");
+let whale = document.querySelector("#whale");
 whale.addEventListener("click", fade);
 
-    function fade () {
-        whale.classList.toggle("fade");
-    }
+function fade() {
+    whale.classList.toggle("fade");
+}
 
-let mirror =document.querySelector("#mirror");
+let mirror = document.querySelector("#mirror");
 mirror.addEventListener("click", kick);
 
-    function kick(){
-        mirror.classList.toggle("kick");
-    }
+function kick() {
+    mirror.classList.toggle("kick");
+}
+
+
+
